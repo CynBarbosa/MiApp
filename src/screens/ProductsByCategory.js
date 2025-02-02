dc909dc53477b35a75cd5b7c6388488a615071d5
cynthia barbosa <cynysole@gmail.com>
2025-02-02 17:06:50 -0300

    realizacion de apk y control de componentes

--- src/screens/ProductsByCategory.js
+++ src/screens/ProductsByCategory.js
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, View, Text } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { useEffect, useState } from "react";
 import Search from "../components/Search";
 import CardProduct from "../components/CardProduct";
